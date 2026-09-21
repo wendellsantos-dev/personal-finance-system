8317468f097c2f97b4e7dbd1ad3b87c6ecce6ae0
Wendell Santos <dev.wendellsantos@gmail.com>
2026-09-21 01:00:13 -0300

    feat: setup Flask Blueprint and add initial index route

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    from app.routes import main
+    app.register_blueprint(main)
     return app
