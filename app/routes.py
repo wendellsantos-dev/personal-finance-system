ad3ee64714f7e594122e65181bfe3d0807a05b48
Wendell Santos <dev.wendellsantos@gmail.com>
2026-09-22 21:51:35 -0300

    feat: render index.html template for Poupa Money home page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template('index.html')
